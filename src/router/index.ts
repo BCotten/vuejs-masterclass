77b85481638b4ce4237ce5d997a816180544f65e
Brandon Cotten <brandon__cotten@hotmail.com>
2024-05-30 17:39:59 -0700

    Vue Router to Dynamic Routes with Wildcards

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,24 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('@/views/ProjectsView.vue')
+    },
+    {
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectView.vue')
+    }
   ]
 })
 
